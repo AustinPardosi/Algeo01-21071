c41ec0df4dd256d2e135760da448537e82821014
Margaretha Olivia <margarethaolivia41@gmail.com>
2022-09-19 05:57:09 +0700

    add readByFile (input from file)

--- src/InputOutput.java
+++ src/InputOutput.java
@@ -1,12 +1,14 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 class InputOutput {
     // Matriks dasar ((sengaja dibuat berlebihan))
-    double [][] Mat = new double[100][100];
+    double[][] Mat = new double[100][100];
 
     // Baca dari keyboard
-    void readByKeyboard (int M, int N) {
-        int i,j;
+    void readByKeyboard(int M, int N) {
+        int i, j;
 
         Scanner scan = new Scanner(System.in);
         for (i = 0; i < M; i++) {
@@ -18,8 +20,8 @@ class InputOutput {
     }
 
     // Print ke layar
-    void printToScreen (int M, int N) {
-        int i,j;
+    void printToScreen(int M, int N) {
+        int i, j;
 
         for (i = 0; i < M; i++) {
             for (j = 0; j < N; j++) {
@@ -28,4 +30,61 @@ class InputOutput {
             System.out.println();
         }
     }
+
+    // Baca dari file
+    public static float[][] readByFile(String filename) {
+        int[] rowCol = countRowCol("filename");
+        float[][] matrix = new float[rowCol[0]][rowCol[1]];
+        int i = 0;
+        int j = 0;
+        try {
+            File myObj = new File("test/" + filename + ".txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                String[] splitStr = data.split("\\s+");
+                j = 0;
+                for (String str : splitStr) {
+                    matrix[i][j] = Float.parseFloat(str);
+                    j += 1;
+                }
+                i += 1;
+            }
+            myReader.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        return matrix;
+    }
+
+    // Hitung row dan col dari file
+    public static int[] countRowCol(String filename) {
+        // mereturn array berisi [row, col];
+        int[] rowCol = new int[2];
+        try {
+            int i = 0;
+            int j = 0;
+            File myObj = new File("test/" + filename + ".txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                String[] splitStr = data.split("\\s+");
+                j = 0;
+                for (String str : splitStr) {
+                    j += 1;
+                }
+                i += 1;
+            }
+            rowCol[0] = i;
+            rowCol[1] = j;
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        return rowCol;
+    }
+
 }
