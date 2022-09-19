c79455aa20c5f13a0efb101739e51d1953a5dd15
Michael Leon Putra Widhi <91791710+mikeleo03@users.noreply.github.com>
2022-09-19 09:37:17 +0700

    drivernyaaaa

---
+++ src/DriverMatriks.java
@@ -0,0 +1,44 @@
+import java.util.Scanner; 
+
+class DriverMatriks {
+    public static void main(String[] args) {
+        // Baca Input
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Masukkan jumlah baris");
+        int baris = sc.nextInt();
+        System.out.println("Masukkan jumlah kolom");
+        int kolom = sc.nextInt();
+
+        // Define matriks
+        double[][] A = new double[baris][kolom];
+
+        // Manggil fungsi eksternal
+        InputOutput M = new InputOutput();
+        Gauss ge = new Gauss();
+
+        // Baca matriks
+        System.out.println("Baca matriks");
+        M.readByKeyboard (A, baris, kolom);
+        
+        // Eselon baris
+        ge.gauss(A, baris, kolom);
+
+        // ini ngitung determinan, GAUSA DIGANTI2 DULU Y
+        /*
+        int kali = 1;
+        for (int i = 0; i < baris; i++) {
+            for (int j = 0; j < kolom; j++) {
+                if (i == j) {
+                    kali *= A[i][j];
+                }
+            }
+        }
+        */
+
+        System.out.println(kali);
+
+        // Cetak matriks
+        System.out.println("Cetak matriks");
+        M.printToScreen (A, baris, kolom);
+	}
+}
