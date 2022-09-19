7627f608535b6db2f8823866ddf7f272175e01fe
Michael Leon Putra Widhi <91791710+mikeleo03@users.noreply.github.com>
2022-09-19 09:22:45 +0700

    Update I/O HEHEHEH LUPA

--- src/InputOutput.java
+++ src/InputOutput.java
@@ -3,29 +3,26 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 class InputOutput {
-    // Matriks dasar ((sengaja dibuat berlebihan))
-    double[][] Mat = new double[100][100];
-
     // Baca dari keyboard
-    void readByKeyboard(int M, int N) {
-        int i, j;
+    void readByKeyboard (double[][] matr, int M, int N) {
+        int i,j;
 
         Scanner scan = new Scanner(System.in);
         for (i = 0; i < M; i++) {
             for (j = 0; j < N; j++) {
-                Mat[i][j] = scan.nextDouble();
+                matr[i][j] = scan.nextDouble();
             }
         }
         scan.close();
     }
 
     // Print ke layar
-    void printToScreen(int M, int N) {
-        int i, j;
+    void printToScreen (double[][] matr, int M, int N) {
+        int i,j;
 
         for (i = 0; i < M; i++) {
             for (j = 0; j < N; j++) {
-                System.out.printf("%.2f ", Mat[i][j]);
+                System.out.printf("%.2f ", matr[i][j]);
             }
             System.out.println();
         }
